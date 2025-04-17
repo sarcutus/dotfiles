81fc545aef3d9def81dac3e3e81108dac3bfb738
sarcutus <58309719+sarcutus@users.noreply.github.com>
2025-04-17 18:47:02 -0400

    Add files via upload

--- home.nix
+++ home.nix
@@ -9,9 +9,11 @@
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # make sure to also set the portal package, so that they are in sync
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    extraConfig = builtins.readFile (/home/sarcutus/.config/hypr/hyprland.conf);
   };
 
-  programs.bash.enable = true;
+  
+  # programs.bash.enable = true;
 
   # The state version is required and should stay at the version you
   # originally installed.
