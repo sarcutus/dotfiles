463b578cc90db7b3f66e093144b83d036cfa445e
sarcutus <sarcutus@github.com>
2026-05-28 08:25:22 -0400

    Niri/Noctalia setup! Whoopeeeeee

---
+++ quickshell/noctalia-shell/Scripts/python/src/calendar/check-calendar.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.13
+import gi
+
+gi.require_version('EDataServer', '1.2')
+gi.require_version('ECal', '2.0')
+
+try:
+    from gi.repository import ECal, EDataServer
+    print("available")
+except ImportError as e:
+    print(f"unavailable: {e}")
